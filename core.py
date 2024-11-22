e42c95de6a2aed627b44e91360f055442cf943ef
Erik Sewell <esewell@berkeley.edu>
2024-11-22 00:58:40 -0800

    Renamed classes.py to core.py and added function for initializing bodies with randomly distributed parameters

---
+++ core.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Body:
+
+    def __init__(self, mass, position, velocity):
+        self.mass = mass # mass of body [kg]
+        self.position = position # position of body [m]
+        self.velocity = velocity # velocity of body [m/s]
+
+# initialize bodies with random masses, positions, and velocities over set range
+# bodies[n] = Body(mass, position [x, y], velocity [vx, vy])
+def initialize_bodies(num_bodies, mass_range, position_range, velocity_range):
+
+    masses = np.random.uniform(mass_range[0], mass_range[1], num_bodies)
+    positions = np.random.uniform(position_range[0], position_range[1], (num_bodies, 2))
+    velocities = np.random.uniform(velocity_range[0], velocity_range[1], (num_bodies, 2))
+
+    # returning list of bodies
+    return [Body(masses[i], positions[i], velocities[i]) for i in range(num_bodies)]
+
+class NBodySimulation:
+
+    # bodies = list of bodies involved in simulation
+    def __init__(self, bodies):
+        self.constants()
+        self.bodies = bodies
+
+    def constants(self):
+        self.G = 6.67430e-11 # gravitational constant [N*m^2/kg^2]
+        self.c = 3e8 # speed of light [m/s]
+
+    # run simulation
