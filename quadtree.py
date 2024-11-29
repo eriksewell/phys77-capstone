8ab6c2a5f12be18fc2fef183b0c677641a133f09
micahkim1117 <micahkim1117@gmail.com>
2024-11-28 22:27:43 -0800

    Quadtree Generation now functional w/ graph

--- quadtree.py
+++ quadtree.py
@@ -3,7 +3,7 @@ from core import Body
 class Node:
 
 #Initializes Nodes. 
-#Points should be a list of all bodies within the bounds (as an index)
+#Points should be a list of all bodies within the bounds (as a list of the body class)
 #Measure position of nodes from center of the node
 #size should be an integer. will determine size of the nodes based upon a fraction of the total size: e.g. (n/2**size)
     def __init__(self, points, nodeposition, size):
@@ -31,45 +31,32 @@ class Node:
         return XCoM, YCoM
 
 
-# bodies should be all of the bodies in the node's quadrant
+# passed variable "bodies" should be all of the bodies in the node's quadrant
+# passed variable "nodelist" is the list of nodes within the quadtree, and is passed to the function for every recursion
+# simsize is consistent, and is modified based on the size of the node
     def subdivide(self, bodies, nodelist, simsize):
-# Detects boundaries for each
 
+# Detects boundaries for each quadrant and sorts the bodies accordingly
         quad1points = []
         quad2points = []
         quad3points = []
         quad4points = []
 
-        # print("subdivide bodies is" + str(bodies))
-        # print('scanned points in node' + str(self.points))
         for i in self.points:
             
             if  bodies[i].position[0] <= self.nodeposition[0]:
                 if bodies[i].position[1] >= self.nodeposition[1]:
-                    # print(str(i) +' sorted into quad1')
                     quad1points.append(i)
                 else:
-                    # print(str(i) +' sorted into quad3')
                     quad3points.append(i)
             else:
                 if bodies[i].position[1] >= self.nodeposition[1]:
-                    # print(str(i) +' sorted into quad2')
                     quad2points.append(i)
                 else:
-                    # print(str(i) +' sorted into quad4')
                     quad4points.append(i)
         
-
-        # quad1bodies = [bodies[i] for i in quad1points]
-        # quad2bodies = [bodies[i] for i in quad2points]
-        # quad3bodies = [bodies[i] for i in quad3points]
-        # quad4bodies = [bodies[i] for i in quad4points]
-
-        # print('quad1bodies'+ str(quad1bodies))
-        # print('quad2bodies'+ str(quad2bodies))
-        # print('quad3bodies'+ str(quad3bodies))
-        # print('quad4bodies'+ str(quad4bodies))
-
+# Appends new child nodes to the nodelist
+# Existing parent node is given the indices to the child node within the nodelist
         nodelist.append(Node(quad1points, [self.nodeposition[0]-(simsize/(2**(self.size+1))), self.nodeposition[1]+(simsize/(2**(self.size+1)))], self.size+1))
         self.quad1 = len(nodelist)-1
         nodelist.append(Node(quad2points, [self.nodeposition[0]+(simsize/(2**(self.size+1))), self.nodeposition[1]+(simsize/(2**(self.size+1)))], self.size+1))
@@ -79,31 +66,26 @@ class Node:
         nodelist.append(Node(quad4points, [self.nodeposition[0]+(simsize/(2**(self.size+1))), self.nodeposition[1]-(simsize/(2**(self.size+1)))], self.size+1))
         self.quad4 = len(nodelist)-1
 
-# checking the quad.
+# Recursive function that checks whether a quadrant has multiple bodies. If it does, runs subdivide. If not, ends that section of the recursion
+# Passed "bodies" variable is all bodies within
     def checkQuad(self, bodies, nodelist, simsize):
-        if len(self.points) == 1 or len(self.points) == 0:
-            print('only 1 point in node or no points. Recursion terminated')
+
+# If this part ever breaks in the future, just make it so that len(self.points) == 1 or len(self.points) == 0
+        if len(self.points) <= 1 :
+
             return
         
         if len(self.points) > 1:
-            self.subdivide(bodies, nodelist, simsize)
-            # print('new node quad 1'+str(nodelist[self.quad1]))
-            # print('new node quad 2'+str(nodelist[self.quad2]))
-            # print('new node quad 3'+str(nodelist[self.quad3]))
-            # print('new node quad 4'+str(nodelist[self.quad4]))
 
-            # print('new recursions')
+            self.subdivide(bodies, nodelist, simsize)
+
+# Calls itself recursively
             nodelist[self.quad1].checkQuad(bodies, nodelist, simsize)
             nodelist[self.quad2].checkQuad(bodies, nodelist, simsize)
             nodelist[self.quad3].checkQuad(bodies, nodelist, simsize)
             nodelist[self.quad4].checkQuad(bodies, nodelist, simsize)
 
-        
 
-# may need to implement some sort of softening, but that comes later
-# will probably create some sort of other force-calculation. Will do subdivisions first
-
-    
 class Quadtree:
     
 # Takes bodies of simulation, and simulation size and generates a quadtree with a list of nodes
@@ -112,7 +94,7 @@ class Quadtree:
     def __init__(self, bodies, simsize):
         self.bodies = bodies
         self.simsize = simsize
-        self.nodelist = [Node(list(range(len(bodies))), [0, 0], 0)]
+        self.nodelist = [Node(list(range(len(bodies))), [0, 0], 1)]
         
         self.nodelist[0].checkQuad(self.bodies, self.nodelist, self.simsize)
    
