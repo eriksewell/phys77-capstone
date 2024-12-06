Fixed force calculation issue and added solar system

core.py
@@ -4,13 +4,13 @@ class Body:
 
     def __init__(self, mass, position, velocity):
         self.mass = mass # mass of body [kg]
-        self.position = np.array(position) # position of body [m]
-        self.velocity = np.array(velocity) # velocity of body [m/s]
-        self.force = np.array([0, 0]) # force on body [N]
+        self.position = np.array(position, dtype=np.float64) # position of body [m]
+        self.velocity = np.array(velocity, dtype=np.float64) # velocity of body [m/s]
+        self.force = np.array([0, 0], dtype=np.float64) # force on body [N]
 
     def calculate_force(self, pos, mass):
         G = 1 # gravitational constant
-        soft = 0.05 # softening parameter
+        soft = 0.1 # softening parameter
 
         # displacement vector from self to other_body
         r = pos - self.position 
@@ -18,7 +18,7 @@ class Body:
         # force vector on self from other_body
         F = (G * mass * self.mass * r) / ((np.linalg.norm(r) + soft)**3)
 
-        self.force = F # update force on body
+        self.force = self.force + F # update force on body
 
 # initialize bodies with random masses, positions, and velocities over set range
 # num_bodies [scalar], mass_range [low, high], position_range [low, high], velocity_range [low, high]